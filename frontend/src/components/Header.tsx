5911237dcf661675e77cba0e79658540f8ab6625
AleksaGrocic <agrocic@gmail.com>
2024-11-30 10:03:47 +0100

    .

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const Header: FC = () => {
+  return (
+    <header className="header">
+      <div className="header_container">
+        <h2 className="title">Web Shop Practice</h2>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
